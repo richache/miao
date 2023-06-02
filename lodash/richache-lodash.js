4218327bc36603d7a240f580e089ed1286848e2d
richache <richardchen2016@163.com>
2023-06-02 17:58:19 +0800

    Update: 更新chunk，compact

--- lodash/richache-lodash.js
+++ lodash/richache-lodash.js
@@ -1,6 +1,26 @@
 var richache = {
+  chunk: function (array, size = 1) {
+    let res = []
+    for (let i = 0, j = 0; i < array.length; i += size, j++) {
+      res[j] = array.slice(i, i + size)
+    }
+    return res
+  },
+
   reverse: function (array) {
     return array.reverse()
-  }
+  },
+
+  compact: function (array) {
+    let res = []
+    for (let i of array) {
+      if (i) {
+        res.push(i)
+      }
+    }
+    return res
+  },
+
+
 
 }
